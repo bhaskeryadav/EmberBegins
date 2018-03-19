4b405382857c5b6e208608e8fd72ef7839857539
Bhasker Yadav <bhaskerpyadav@gmail.com>
2018-03-19 17:37:54 +0530

    color change

--- myApp/app/controllers/index.js
+++ myApp/app/controllers/index.js
@@ -1,6 +1,20 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-	name: "Ember Controller"
+  edit: true,  // make data visible.
+  color:"",
+  color2:"",
+  color3:"",
+  colors:['red','green','thrdColor','fourthColor','fithColor'],
+  clrIndex:0,
+  actions:{ // trigger an event on the click.
+    changeEdit: function(){
+      console.log("changeEdit @ controller "+this.edit) ;
+      this.toggleProperty('edit');
+      
+      this.set('color2',this.colors[(this.clrIndex+1)%this.colors.length]);
+      this.set('color3',this.colors[(this.clrIndex+2)%this.colors.length]);
+      this.set('color',this.colors[this.clrIndex++%this.colors.length]);
+    }
+  }
 });
-console.log("in Controller.") ;
--- myApp/app/routes/index.js
+++ myApp/app/routes/index.js
@@ -1,13 +1,30 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-	model: function() {
-		console.log("model fn @ Route.") ;
-		return {
-            name:'bhasker',
-            values:['Router', 'Model', 'View', 'Controller']
-        };
-    },
+	// model: function() {
+	// 	console.log("model fn @ Route.") ;
+	// 	return ['Router', 'Model', 'View', 'Controller'];
+        
+    // },
+
+    model: function() {
+        console.log("model @ Route") ;
+    
+        var companies = [{
+          "name" : "Google",
+          "headquarters": "Mountain View",
+          "revenue":"59b"
+          },{
+            "name" : "Facebook",
+            "headquarters":"Menlo Park",
+            "revenue":"7b"
+          },{
+            "name" : "twitter",
+            "revenue": "664m",
+            "headquarters":"San Francisco"
+          }];
+        return companies;
+      }
    
 });
 console.log("in Route.") ;
